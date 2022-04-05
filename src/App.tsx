9f20c48c159248fe85ef4b83e1013b8ff9f71b25
Tao Ning <tao.ning>
2022-04-05 16:34:50 -0700

    Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeatMap from "./projects/heatmap/scripts/containers/HeatMap";
+import SearchBar from "./projects/searchBar/components/SearchBar";
 
 function App() {
   return (
     <div className="App">
-        <HeatMap />
+        <SearchBar />
     </div>
   );
 }
