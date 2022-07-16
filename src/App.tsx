8aac14b2f69dad2161b406a2fd5874e81603805b
ntgd1102 <ntgd1102@gmail.com>
2022-07-16 13:25:23 -0700

    save

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {run} from './projects/env'; // eslint-disable-next-line max-len
-import {MediaCardDemo} from './projects/mediaCard/components/MediaCardDemo';
+import { run } from './projects/env'; // eslint-disable-next-line max-len
+import { MediaCardDemo } from './projects/mediaCard/components/MediaCardDemo';
 
 const App = () => {
   run();
