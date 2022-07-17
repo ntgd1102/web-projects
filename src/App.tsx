a8595320994d8ae95c788f1423ea96bd181617ba
ntgd1102 <ntgd1102@gmail.com>
2022-07-16 17:28:04 -0700

    save

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { run } from './projects/env'; // eslint-disable-next-line max-len
-import { MediaCardDemo } from './projects/mediaCard/components/MediaCardDemo';
+import { CountDown } from './projects/countDown/CountDown';
 
 const App = () => {
   run();
   return (
     <div className="App">
-      <MediaCardDemo />
+      <CountDown />
     </div>
   );
 };
