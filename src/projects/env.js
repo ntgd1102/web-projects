5fa9666588017f50713528f63da7b503cf035f0d
ntgd1102 <ntgd1102@gmail.com>
2022-07-20 10:01:52 -0700

    save

--- src/projects/env.js
+++ src/projects/env.js
@@ -1,7 +1,35 @@
 export const run = () => {
-  const toBeReplaced = 'sdsd    dsds   ds';
-  console.log(toBeReplaced.replace(/\s+/g, ' '));
+  // const toBeReplaced = 'sdsd    dsds   ds';
+  // console.log(toBeReplaced.replace(/\s+/g, ' '));
   // output is "sdsd dsds ds" g: 是全局搜索多个 \s：是空格 +: 一个或多个
+
+  variableToBeChecked.constructor === Array;
+  const extend = function(obj, extension) {
+    if (typeof obj === 'object' && extension.constructor === Array) {
+      for (const i in extension) {
+        if (extension.hasOwnProperty(i) && !obj.hasOwnProperty(i)) {
+          obj[i] = extension[i];
+        }
+      }
+      return obj;
+    }
+  };
+
+  extend({ a: 'b' }, [2, 3, 4]);
+};
+
+const isPalindrome = function(s) {
+  if (s.length <= 1) return true;
+  // process the string
+  const str = s.replace(/[^a-z0-9A-Z]/gi, '').toLowerCase();
+  const left = 0;
+  const right = str.length - 1;
+  if (str[left] === str[right]) {
+    return isPalindrome(s.slice(1, s.length - 1));
+  } else {
+    return false;
+  }
+  return true;
 };
 
 export default run;
